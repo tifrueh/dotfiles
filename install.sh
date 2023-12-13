6d8c71d24cebb2fab7394f2ac045b5c5e8c1988a
Timo Früh <timo.frueh@icloud.com>
2023-12-13 22:12:48 +0100

    Improve install.sh drastically
    * Use /bin/zsh instead of /bin/sh (as zsh is a prerequisite for the
      whole setup)
    * Always execute script in its directory
    * Split link() into:
      * link(): handles the protected symlinking
      * link_dot(): uses link() to link a dotfile to $HOME
      * -> This was done to facilitate the linking of ~/.config/nvim/init.vim
    * Remove the '--brew' option
    * Rename option '--pyenv' -> '--install-pyenv'
    * Rename option '--nvim' -> '--link-nvim'
    * Move the installation of pyenv and the linking of nvim to the end of
      the script
    * Use "${...}" for variables in (almost) all cases
    * Use [[ ... ]] for conditions (as we're using zsh now)
    * Remove unnecessary '...' at the end of some messages

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/zsh
 
 # store input arguments in variable
 input_args=$*
 
-# store script path in variable
-script_dirname=$(cd "$(dirname "$0")" > /dev/null 2> /dev/null && pwd)
+# always execute script in its directory
+dirpath="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+cd "${dirpath}"
 
 # function for checking whether input argument was given
 input_contains () {
-	seeking=$1
+	seeking=${1}
 	in=1
-	for element in $input_args; do
-		if [ "$element" = "$seeking" ]; then
+	for element in "${input_args}"; do
+		if [[ "${element}" = "${seeking}" ]]; then
 			in=0
 			break
 		fi
@@ -19,41 +20,57 @@ input_contains () {
 	return $in
 }
 
-# function for linking a dotfile
+# function for protected symlinking
+#
+# Synopsis
+#   link FILENAME DESTINATION_PATH
+#
+# Description
+#   FILENAME		The file's name in the dotfile directory
+#   DESTINATION_PATH	The file's destination path
 link () {
-	if ! [ -e $HOME/.$1 ]; then
-		echo "DOT-INSTALL: Linking $1 ..."
-		ln -s $script_dirname/$1 $HOME/.$1
-	elif input_contains '--force-links'; then
-		echo -n "Do you really want to overwrite your existing $1? [y|n] "
+	if [[ -h "${2}" || -f "${2}" ]] && input_contains "--force-links"; then
+		echo -n "Do you really want to overwrite ${2}? [y|n] "
 		read confirm
-		if [ $confirm = "y" ]; then
-			echo "DOT-INSTALL: Removing existing $1 ..."
-			rm $HOME/.$1
-			echo "DOT-INSTALL: Linking $1 ..."
-			ln -s $script_dirname/$1 $HOME/.$1
+		if [[ "${confirm}" = "y" ]]; then
+			echo "DOT-INSTALL: Overwriting ${2} <- ${1}"
+			rm "${2}"
+			ln -s "${dirpath}/${1}" "${2}"
 		else
-			echo "DOT-INSTALL: SKIP: Not overwriting $1, skipping ..."
+			echo "DOT-INSTALL: SKIP: Not overwriting ${2}, skipping ..."
 		fi
+	elif [[ -h "${2}" || -f "${2}" ]]; then
+		echo "DOT-INSTALL: ${2} exists, skipping (override with '--force-links') ..."
+	elif [[ -e "${2}" ]]; then
+		echo "DOT-INSTALL: ${2} exists and is not a symbolic link or regular file, skipping ..."
 	else
-		echo "DOT-INSTALL: SKIP: Existing $1 found, skipping ..."
+		echo "DOT-INSTALL: Linking ${1} -> ${2}"
+		ln -s "${dirpath}/${1}" "${2}"
 	fi
 }
 
+# function for linking a dotfile
+#
+# Synopsis
+#   link_dot FILENAME
+# 
+# Description
+#   FILENAME		The file's name in the dotfile directory
+link_dot () {
+	link "${1}" "${HOME}/.${1}"
+}
+
+# provide help if requested
 if input_contains '-h' || input_contains '--help'; then
 	echo "Possible options:"
-	echo "--brew:        Install HOMEBREW"
-	echo "--pyenv:       Install PYENV"
-	echo "--nvim:        Link NVIM config"
-	echo "--force-links: Overwrite existing dotfiles"
-	echo "--exec-zsh:    Execute ZSH after completion"
+	echo "  --force-links:      Overwrite existing dotfiles"
+	echo "  --link-nvim:        Link NVIM config"
+	echo "  --install-pyenv:    Install PYENV"
+	echo "  --exec-zsh:         Execute ZSH after completion"
 	exit 0
 fi
 
-# store nvim config location in variable
-nvim_rc="${HOME}/.config/nvim/init.vim"
-
-# enforce requirements
+# enforce requirements zsh curl and git
 if ! type zsh > /dev/null 2> /dev/null; then
 	echo "DOT-INSTALL: ERROR: Please install ZSH before executing this script ..."
 	exit 1
@@ -69,128 +86,92 @@ if ! type git > /dev/null 2> /dev/null; then
 	exit 1
 fi
 
-# install homebrew if requested, if on macOS and if not already installed
-if input_contains '--brew' && type brew > /dev/null 2> /dev/null; then
-	echo "DOT-INSTALL: ERROR: HOMEBREW already installed ..."
-	exit 1
-elif input_contains '--brew' && ! [ "$OSTYPE" = "darwin" ]; then
-	echo "DOT-INSTALL: ERROR: HOMEBREW cannot be installed on any other system than macOS ..."
-	exit 1
-elif input_contains '--brew' && [ "$OSTYPE" = "darwin" ] && ! type brew > /dev/null 2> /dev/null; then
-	echo "DOT-INSTALL: Installing HOMEBREW ..."
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# install pyenv if requested, if on macOS and if not already installed
-if input_contains '--pyenv' && [ -d $HOME/.pyenv ]; then
-	echo "DOT-INSTALL: ERROR: PYENV already installed ..."
-	exit 1
-elif input_contains '--pyenv' && ! [ -d $HOME/.pyenv ]; then
-	echo "DOT-INSTALL: Installing PYENV ..."
-	git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
-	echo "DOT-INSTALL: Intalling PYENV-VIRTUALENV  ..."
-	git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
-fi
-
-# link nvim config if requested and no config is present
-if input_contains '--nvim' && [ ! -e $nvim_rc ]; then
-	mkdir -p $HOME/.config/nvim
-	ln -s $script_dirname/nvimrc $nvim_rc
-elif input_contains '--force-links'; then
-	echo -n "Do you really want to overwrite your existing nvim config? [y|n] "
-	read confirm
-	if [ $confirm = "y" ]; then
-		echo "DOT-INSTALL: Removing existing nvim config ..."
-		rm $nvim_rc
-		echo "DOT-INSTALL: Linking nvim config ..."
-		ln -s $script_dirname/nvimrc $nvim_rc
-	else
-		echo "DOT-INSTALL: SKIP: Not overwriting nvim config, skipping ..."
-	fi
-fi
-
 # create .zsh directory
-if ! [ -d $HOME/.zsh ]; then
-	echo "DOT-INSTALL: Creating .ZSH DIRECTORY ..."
-	mkdir -p $HOME/.zsh/plugins $HOME/.zsh/themes
+if ! [[ -d "${HOME}/.zsh" ]]; then
+	echo "DOT-INSTALL: Creating .ZSH DIRECTORY"
+	mkdir -p "${HOME}/.zsh/plugins ${HOME}/.zsh/themes"
 
-	if [ -f $HOME/.zsh_history ]; then
-		mv $HOME/.zsh_history $HOME/.zsh/
+	if [[ -f "${HOME}/.zsh_history" ]]; then
+		mv "${HOME}/.zsh_history" "${HOME}/.zsh/"
 	else
-		touch $HOME/.zsh/.zsh_history
+		touch "${HOME}/.zsh/.zsh_history"
 	fi
 
 else
 	echo "DOT-INSTALL: SKIP: .ZSH DIRECTORY already created, skipping ..."
 fi
 
-# install vim if on macOS or a Debian based Linux, ask for manual installation if not
-if ! type vim > /dev/null 2> /dev/null && $(uname) = "Darwin"; then
-	brew install vim
-elif ! type vim > /dev/null 2> /dev/null && [ -f /etc/debian_version ]; then
-	sudo apt install vim
-elif ! type vim > /dev/null 2> /dev/null; then
-	echo "DOT-INSTALL: ERROR: Unsupported OS for automatic VIM installation ..."
-	echo "DOT-INSTALL: =====> Please install VIM and restart the script ..."
-	exit 1
-fi
-
 # install vim-plug if not already installed
-if ! [ -f $HOME/.vim/autoload/plug.vim ]; then
-	echo "DOT-INSTALL: Installing VIM-PLUG ..."
-	curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if ! [[ -f "${HOME}/.vim/autoload/plug.vim" ]]; then
+	echo "DOT-INSTALL: Installing VIM-PLUG"
+	curl -fLo "${HOME}/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 else
 	echo "DOT-INSTALL: SKIP: VIM-PLUG already installed, skipping ..."
 fi
 
 # install pure if not already installed
-if ! [ -d $HOME/.zsh/themes/pure ]; then
-	echo "DOT-INSTALL: Installing PURE ..."
-	git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/themes/pure"
+if ! [[ -d "${HOME}/.zsh/themes/pure" ]]; then
+	echo "DOT-INSTALL: Installing PURE"
+	git clone https://github.com/sindresorhus/pure.git "${HOME}/.zsh/themes/pure"
 else
 	echo "DOT-INSTALL: SKIP: PURE already installed, skipping ..."
 fi
 
 # install p10k if not already installed
-if ! [ -d $HOME/.zsh/themes/p10k ]; then
-	echo "DOT-INSTALL: Installing P10K ..."
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.zsh/themes/p10k"
+if ! [[ -d "${HOME}/.zsh/themes/p10k" ]]; then
+	echo "DOT-INSTALL: Installing P10K"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${HOME}/.zsh/themes/p10k"
 else
 	echo "DOT-INSTALL: SKIP: P10K already installed, skipping ..."
 fi
 
 # install zsh-syntax-highlighting if not already installed
-if ! [ -d $HOME/.zsh/plugins/zsh-syntax-highlighting ]; then
-	echo "DOT-INSTALL: Installing ZSH-SYNTAX-HIGHLIGHTING ..."
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh/plugins/zsh-syntax-highlighting"
+if ! [[ -d "${HOME}/.zsh/plugins/zsh-syntax-highlighting" ]]; then
+	echo "DOT-INSTALL: Installing ZSH-SYNTAX-HIGHLIGHTING"
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.zsh/plugins/zsh-syntax-highlighting"
 else
 	echo "DOT-INSTALL: SKIP: ZSH-SYNTAX-HIGHLIGHTING already installed, skipping ..."
 fi
 
-if ! [ -d $HOME/.zsh/plugins/zsh-completions ]; then
-	echo "DOT-INSTALL: Installing ZSH-COMPLETIONS ..."
-	git clone https://github.com/zsh-users/zsh-completions.git "$HOME/.zsh/plugins/zsh-completions"
+# install zsh-completions if not already installed
+if ! [[ -d "${HOME}/.zsh/plugins/zsh-completions" ]]; then
+	echo "DOT-INSTALL: Installing ZSH-COMPLETIONS"
+	git clone https://github.com/zsh-users/zsh-completions.git "${HOME}/.zsh/plugins/zsh-completions"
 else
 	echo "DOT-INSTALL: SKIP: ZSH-COMPLETIONS already installed, skipping ..."
 fi
 
-if ! [ -d $HOME/.zsh/plugins/zsh-history-substring-search ]; then
-	echo "DOT-INSTALL: Installing ZSH-HISTORY-SUBSTRING-SEARCH ..."
+# install zsh-history-substring-search if not already installed
+if ! [[ -d "${HOME}/.zsh/plugins/zsh-history-substring-search" ]]; then
+	echo "DOT-INSTALL: Installing ZSH-HISTORY-SUBSTRING-SEARCH"
 	git clone https://github.com/zsh-users/zsh-history-substring-search.git "${HOME}/.zsh/plugins/zsh-history-substring-search"
 else
 	echo "DOT-INSTALL: SKIP: ZSH-HISTORY-SUBSTRING-SEARCH already installed, skipping ..."
 fi
 
-# add symlinks if they do not exist already and overwrite them if requested
-link "zshrc"
-link "zprofile"
-link "zshaliases"
-link "vimrc"
-link "nethackrc"
+# link nvim config if requested
+if input_contains "--link_nvim"; then
+	link "nvimrc" "${HOME}/.config/nvim/init.vim"
+fi
 
-if input_contains '--pyenv'; then
+# install pyenv if requested, if on macOS and if not already installed
+if input_contains "--install-pyenv" && [[ -d "${HOME}/.pyenv" ]]; then
+	echo "DOT-INSTALL: ERROR: PYENV already installed ..."
+	exit 1
+elif input_contains "--install-pyenv"; then
+	echo "DOT-INSTALL: Installing PYENV"
+	git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
+	echo "DOT-INSTALL: Intalling PYENV-VIRTUALENV"
+	git clone https://github.com/pyenv/pyenv-virtualenv.git "${HOME}/.pyenv/plugins/pyenv-virtualenv"
 	echo "DOT-INSTALL: NOTIFICATION: PYENV was installed, but please install all Python dependencies before installing Python ..."
 fi
+
+# link all dotfiles
+link_dot "zshrc"
+link_dot "zprofile"
+link_dot "zshaliases"
+link_dot "vimrc"
+link_dot "nethackrc"
 
 if input_contains '--exec-zsh'; then
 	exec zsh
