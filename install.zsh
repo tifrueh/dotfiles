b072b19951407fcd046c2c2996943f9cf7da0238
Timo Früh <timo.frueh@icloud.com>
2024-05-14 12:52:19 +0200

    Use variables for directory paths

--- install.zsh
+++ install.zsh
@@ -1,5 +1,18 @@
 #!/bin/zsh
 
+# define path variables
+zsh_dir="${HOME}/.zsh"
+zsh_plugin_dir="${zsh_dir}/plugins"
+zsh_themes_dir="${zsh_dir}/themes"
+vim_dir="${HOME}/.vim"
+vim_colors_dir="${vim_dir}/colors"
+vim_plugins_dir="${vim_dir}/pack/plugins/start"
+config_dir="${HOME}/.config"
+nvim_dir="${config_dir}/nvim"
+nvim_colors_dir="${nvim_dir}/colors"
+nvim_plugins_dir="${nvim_dir}/pack/plugins/start"
+kitty_dir="${config_dir}/kitty"
+
 # store input arguments in variable
 zparseopts -E -D -a input_args -- \
     h \
@@ -101,7 +114,7 @@ fi
 # create .zsh directory
 if ! [[ -d "${HOME}/.zsh" ]]; then
     echo "DOT-INSTALL: Creating .ZSH DIRECTORY"
-    mkdir -p "${HOME}/.zsh/plugins ${HOME}/.zsh/themes"
+    mkdir -p "${zsh_plugin_dir}" "${zsh_themes_dir}"
 
     if [[ -f "${HOME}/.zsh_history" ]]; then
         mv "${HOME}/.zsh_history" "${HOME}/.zsh/"
@@ -114,59 +127,59 @@ else
 fi
 
 # install pure if not already installed
-if ! [[ -d "${HOME}/.zsh/themes/pure" ]]; then
+if ! [[ -d "${zsh_themes_dir}/pure" ]]; then
     echo "DOT-INSTALL: Installing PURE"
-    git clone https://github.com/sindresorhus/pure.git "${HOME}/.zsh/themes/pure"
+    git clone https://github.com/sindresorhus/pure.git "${zsh_themes_dir}/pure"
 else
     echo "DOT-INSTALL: SKIP: PURE already installed, skipping ..."
 fi
 
 # install p10k if not already installed
-if ! [[ -d "${HOME}/.zsh/themes/p10k" ]]; then
+if ! [[ -d "${zsh_themes_dir}/p10k" ]]; then
     echo "DOT-INSTALL: Installing P10K"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${HOME}/.zsh/themes/p10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${zsh_themes_dir}/p10k"
 else
     echo "DOT-INSTALL: SKIP: P10K already installed, skipping ..."
 fi
 
 # install zsh-syntax-highlighting if not already installed
-if ! [[ -d "${HOME}/.zsh/plugins/zsh-syntax-highlighting" ]]; then
+if ! [[ -d "${zsh_plugin_dir}/zsh-syntax-highlighting" ]]; then
     echo "DOT-INSTALL: Installing ZSH-SYNTAX-HIGHLIGHTING"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.zsh/plugins/zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${zsh_plugin_dir}/zsh-syntax-highlighting"
 else
     echo "DOT-INSTALL: SKIP: ZSH-SYNTAX-HIGHLIGHTING already installed, skipping ..."
 fi
 
 # install zsh-history-substring-search if not already installed
-if ! [[ -d "${HOME}/.zsh/plugins/zsh-history-substring-search" ]]; then
+if ! [[ -d "${zsh_plugin_dir}/zsh-history-substring-search" ]]; then
     echo "DOT-INSTALL: Installing ZSH-HISTORY-SUBSTRING-SEARCH"
-    git clone https://github.com/zsh-users/zsh-history-substring-search.git "${HOME}/.zsh/plugins/zsh-history-substring-search"
+    git clone https://github.com/zsh-users/zsh-history-substring-search.git "${zsh_plugin_dir}/zsh-history-substring-search"
 else
     echo "DOT-INSTALL: SKIP: ZSH-HISTORY-SUBSTRING-SEARCH already installed, skipping ..."
 fi
 
 # install the onehalf colorscheme for vim
-if ! [[ -f "${HOME}/.vim/colors/onehalfdark.vim" ]]; then
+if ! [[ -f "${vim_colors_dir}/onehalfdark.vim" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFDARK for VIM"
-    mkdir -p "${HOME}/.vim/colors"
-    curl -sSL https://github.com/sonph/onehalf/raw/master/vim/colors/onehalfdark.vim -o "${HOME}/.vim/colors/onehalfdark.vim"
+    mkdir -p "${vim_colors_dir}"
+    curl -sSL https://github.com/sonph/onehalf/raw/master/vim/colors/onehalfdark.vim -o "${vim_colors_dir}/onehalfdark.vim"
 else
     echo "DOT-INSTALL: SKIP: ONEHALFDARK for VIM already installed, skipping ..."
 fi
 
-if ! [[ -f "${HOME}/.vim/colors/onehalflight.vim" ]]; then
+if ! [[ -f "${vim_colors_dir}/onehalflight.vim" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFLIGHT for VIM"
-    mkdir -p "${HOME}/.vim/colors"
-    curl -sSL https://github.com/sonph/onehalf/raw/master/vim/colors/onehalflight.vim -o "${HOME}/.vim/colors/onehalflight.vim"
+    mkdir -p "${vim_colors_dir}"
+    curl -sSL https://github.com/sonph/onehalf/raw/master/vim/colors/onehalflight.vim -o "${vim_colors_dir}/onehalflight.vim"
 else
     echo "DOT-INSTALL: SKIP: ONEHALFLIGHT for VIM already installed, skipping ..."
 fi
 
 # install the lightline plugin for vim
-if ! [[ -d "${HOME}/.vim/pack/plugins/start/lightline" ]]; then
+if ! [[ -d "${vim_plugins_dir}/lightline" ]]; then
     echo "DOT-INSTALL: Installing LIGHTLINE for VIM"
-    mkdir -p "${HOME}/.vim/pack/plugins/start"
-    git clone https://github.com/itchyny/lightline.vim "${HOME}/.vim/pack/plugins/start/lightline"
+    mkdir -p "${vim_plugins_dir}"
+    git clone https://github.com/itchyny/lightline.vim "${vim_plugins_dir}/lightline"
 else
     echo "DOT-INSTALL: SKIP: LIGHTLINE for VIM already installed, skipping ..."
 fi
@@ -181,57 +194,57 @@ link_dot "zshrc"
 # configure nvim if requested
 ## link nvimrc and copy vim colors
 if input_contains "--configure-nvim"; then
-    mkdir -p "${HOME}/.config/nvim"
-    link "nvimrc" "${HOME}/.config/nvim/init.lua"
+    mkdir -p "${nvim_dir}"
+    link "nvimrc" "${nvim_dir}/init.lua"
     echo "DOT-INSTALL: Copying VIM colorschemes to NVIM"
-    mkdir -p "${HOME}/.config/nvim/colors"
-    cp "${HOME}/.vim/colors/"* "${HOME}/.config/nvim/colors"
+    mkdir -p "${nvim_colors_dir}/colors"
+    cp "${vim_colors_dir}/"* "${nvim_colors_dir}/"
 fi
 
 ## install the nvim-web-devicon plugin for nvim
-if input_contains "--configure-nvim" && [[ ! -d "${HOME}/.config/nvim/pack/plugins/start/nvim-web-devicons" ]]; then
+if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-web-devicons" ]]; then
     echo "DOT-INSTALL: Installing NVIM-WEB-DEVICONS for NVIM"
-    git clone https://github.com/nvim-tree/nvim-web-devicons "${HOME}/.config/nvim/pack/plugins/start/nvim-web-devicons"
+    git clone https://github.com/nvim-tree/nvim-web-devicons "${nvim_plugins_dir}/nvim-web-devicons"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-WEB-DEVICONS for NVIM already installed, skipping ..."
 fi
 
 ## install the lualine plugin for nvim
-if input_contains "--configure-nvim" && [[ ! -d "${HOME}/.config/nvim/pack/plugins/start/lualine" ]]; then
+if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/lualine" ]]; then
     echo "DOT-INSTALL: Installing LUALINE for NVIM"
-    git clone https://github.com/nvim-lualine/lualine.nvim.git "${HOME}/.config/nvim/pack/plugins/start/lualine"
+    git clone https://github.com/nvim-lualine/lualine.nvim.git "${nvim_plugins_dir}/lualine"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: LUALINE for NVIM already installed, skipping ..."
 fi
 
 ## install the nvim-surround plugin for nvim
-if input_contains "--configure-nvim" && [[ ! -d "${HOME}/.config/nvim/pack/plugins/start/nvim-surround" ]]; then
+if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-surround" ]]; then
     echo "DOT-INSTALL: Installing NVIM-SURROUND for NVIM"
-    git clone https://github.com/kylechui/nvim-surround.git "${HOME}/.config/nvim/pack/plugins/start/nvim-surround"
+    git clone https://github.com/kylechui/nvim-surround.git "${nvim_plugins_dir}/nvim-surround"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-SURROUND for NVIM already installed, skipping ..."
 fi
 
 ## install the nvim-tree plugin for nvim
-if input_contains "--configure-nvim" && [[ ! -d "${HOME}/.config/nvim/pack/plugins/start/nvim-tree" ]]; then
+if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-tree" ]]; then
     echo "DOT-INSTALL: Installing NVIM-TREE for NVIM"
-    git clone https://github.com/nvim-tree/nvim-tree.lua.git "${HOME}/.config/nvim/pack/plugins/start/nvim-tree"
+    git clone https://github.com/nvim-tree/nvim-tree.lua.git "${nvim_plugins_dir}/nvim-tree"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-TREE for NVIM already installed, skipping ..."
 fi
 
 ## install the nvim-lspconfig plugin for nvim
-if input_contains "--configure-nvim" && [[ ! -d "${HOME}/.config/nvim/pack/plugins/start/nvim-lspconfig" ]]; then
+if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-lspconfig" ]]; then
     echo "DOT-INSTALL: Installing NVIM-LSPCONFIG for NVIM"
-    git clone https://github.com/neovim/nvim-lspconfig.git "${HOME}/.config/nvim/pack/plugins/start/nvim-lspconfig"
+    git clone https://github.com/neovim/nvim-lspconfig.git "${nvim_plugins_dir}/nvim-lspconfig"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-LSPCONFIG for NVIM already installed, skipping ..."
 fi
 
 ## install the nvim-treesitter plugin for neovim
-if input_contains "--configure-nvim" && [[ ! -d "${HOME}/.config/nvim/pack/plugins/start/nvim-treesitter" ]]; then
+if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-treesitter" ]]; then
     echo "DOT-INSTALL: Installing NVIM-TREESITTER for NVIM"
-    git clone https://github.com/nvim-treesitter/nvim-treesitter.git "${HOME}/.config/nvim/pack/plugins/start/nvim-treesitter"
+    git clone https://github.com/nvim-treesitter/nvim-treesitter.git "${nvim_plugins_dir}/nvim-treesitter"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-TREESITTER for NVIM already installed, skipping ..."
 fi
@@ -239,58 +252,58 @@ fi
 # configure kitty if requested
 ## link kitty config
 if input_contains "--configure-kitty"; then
-    mkdir -p "${HOME}/.config/kitty"
-    link "kitty.conf" "${HOME}/.config/kitty/kitty.conf"
-    link "kitty-if.conf" "${HOME}/.config/kitty/if.conf"
+    mkdir -p "${kitty_dir}"
+    link "kitty.conf" "${kitty_dir}/kitty.conf"
+    link "kitty-if.conf" "${kitty_dir}/if.conf"
 fi
 
 ## install onehalfdark for kitty
-if input_contains "--configure-kitty" && [[ ! -f "${HOME}/.config/kitty/onehalfdark.conf" ]]; then
+if input_contains "--configure-kitty" && [[ ! -f "${kitty_dir}/onehalfdark.conf" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFDARK for KITTY"
-    curl -fsSL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-dark.conf" -o "${HOME}/.config/kitty/onehalfdark.conf"
+    curl -fsSL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-dark.conf" -o "${kitty_dir}/onehalfdark.conf"
 elif input_contains "--configure-kitty"; then
     echo "DOT-INSTALL: SKIP: ONEHALFDARK for KITTY already installed, skipping ..."
 fi
 
 ## install onehalfdark for kitty
-if input_contains "--configure-kitty" && [[ ! -f "${HOME}/.config/kitty/onehalflight.conf" ]]; then
+if input_contains "--configure-kitty" && [[ ! -f "${kitty_dir}/onehalflight.conf" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFLIGHT for KITTY"
-    curl -fsSL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-light.conf" -o "${HOME}/.config/kitty/onehalflight.conf"
+    curl -fsSL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-light.conf" -o "${kitty_dir}/onehalflight.conf"
 elif input_contains "--configure-kitty"; then
     echo "DOT-INSTALL: SKIP: ONEHALFLIGHT for KITTY already installed, skipping ..."
 fi
 
 ## install font configuration for kitty
-if input_contains "--configure-kitty" && [[ ! -f "${HOME}/.config/kitty/fontconfig.conf" ]]; then
+if input_contains "--configure-kitty" && [[ ! -f "${kitty_dir}/fontconfig.conf" ]]; then
     echo "DOT-INSTALL: Installing default font configuration for KITTY"
-    cp "${dirpath}/kitty-fontconfig.conf" "${HOME}/.config/kitty/fontconfig.conf"
+    cp "${dirpath}/kitty-fontconfig.conf" "${kitty_dir}/fontconfig.conf"
 elif input_contains "--configure-kitty"; then
     echo "DOT-INSTALL: SKIP: Font configuration file for KITTY found, skipping ..."
 fi
 
 # install hyprland configuration if requested
 if input_contains "--configure-hyprland"; then
-    mkdir -p "${HOME}/.config/hypr"
-    link "hyprland.conf" "${HOME}/.config/hypr/hyprland.conf"
+    mkdir -p "${config_dir}/hypr"
+    link "hyprland.conf" "${config_dir}/hypr/hyprland.conf"
 fi
 
 # install waybar configuration if requested
 if input_contains "--configure-waybar"; then
-    mkdir -p "${HOME}/.config/waybar"
-    link "waybar-config" "${HOME}/.config/waybar/config"
-    link "waybar-style.css" "${HOME}/.config/waybar/style.css"
+    mkdir -p "${config_dir}/waybar"
+    link "waybar-config" "${config_dir}/waybar/config"
+    link "waybar-style.css" "${config_dir}/waybar/style.css"
 fi
 
 # install yabai configuration if requested
 if input_contains "--configure-yabai"; then
-    mkdir -p "${HOME}/.config/yabai"
-    link "yabairc" "${HOME}/.config/yabai/yabairc"
+    mkdir -p "${config_dir}/yabai"
+    link "yabairc" "${config_dir}/yabai/yabairc"
 fi
 
 # install skhd configuration if requested
 if input_contains "--configure-skhd"; then
-    mkdir -p "${HOME}/.config/skhd"
-    link "skhdrc" "${HOME}/.config/skhd/skhdrc"
+    mkdir -p "${config_dir}/skhd"
+    link "skhdrc" "${config_dir}/skhd/skhdrc"
 fi
 
 # install pyenv if requested, if on macOS and if not already installed
