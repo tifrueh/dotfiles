ad61bd79f14265a51f6641cb985dc3e321e7b1af
Timo Früh <timo.frueh@icloud.com>
2024-05-14 12:56:48 +0200

    Refactoring
    - Put URLs in quotes
    - Use -fL when downloading with curl

--- install.zsh
+++ install.zsh
@@ -129,7 +129,7 @@ fi
 # install pure if not already installed
 if ! [[ -d "${zsh_themes_dir}/pure" ]]; then
     echo "DOT-INSTALL: Installing PURE"
-    git clone https://github.com/sindresorhus/pure.git "${zsh_themes_dir}/pure"
+    git clone "https://github.com/sindresorhus/pure.git" "${zsh_themes_dir}/pure"
 else
     echo "DOT-INSTALL: SKIP: PURE already installed, skipping ..."
 fi
@@ -137,7 +137,7 @@ fi
 # install p10k if not already installed
 if ! [[ -d "${zsh_themes_dir}/p10k" ]]; then
     echo "DOT-INSTALL: Installing P10K"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${zsh_themes_dir}/p10k"
+    git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "${zsh_themes_dir}/p10k"
 else
     echo "DOT-INSTALL: SKIP: P10K already installed, skipping ..."
 fi
@@ -145,7 +145,7 @@ fi
 # install zsh-syntax-highlighting if not already installed
 if ! [[ -d "${zsh_plugin_dir}/zsh-syntax-highlighting" ]]; then
     echo "DOT-INSTALL: Installing ZSH-SYNTAX-HIGHLIGHTING"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${zsh_plugin_dir}/zsh-syntax-highlighting"
+    git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${zsh_plugin_dir}/zsh-syntax-highlighting"
 else
     echo "DOT-INSTALL: SKIP: ZSH-SYNTAX-HIGHLIGHTING already installed, skipping ..."
 fi
@@ -153,7 +153,7 @@ fi
 # install zsh-history-substring-search if not already installed
 if ! [[ -d "${zsh_plugin_dir}/zsh-history-substring-search" ]]; then
     echo "DOT-INSTALL: Installing ZSH-HISTORY-SUBSTRING-SEARCH"
-    git clone https://github.com/zsh-users/zsh-history-substring-search.git "${zsh_plugin_dir}/zsh-history-substring-search"
+    git clone "https://github.com/zsh-users/zsh-history-substring-search.git" "${zsh_plugin_dir}/zsh-history-substring-search"
 else
     echo "DOT-INSTALL: SKIP: ZSH-HISTORY-SUBSTRING-SEARCH already installed, skipping ..."
 fi
@@ -162,7 +162,7 @@ fi
 if ! [[ -f "${vim_colors_dir}/onehalfdark.vim" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFDARK for VIM"
     mkdir -p "${vim_colors_dir}"
-    curl -sSL https://github.com/sonph/onehalf/raw/master/vim/colors/onehalfdark.vim -o "${vim_colors_dir}/onehalfdark.vim"
+    curl -fL "https://github.com/sonph/onehalf/raw/master/vim/colors/onehalfdark.vim" -o "${vim_colors_dir}/onehalfdark.vim"
 else
     echo "DOT-INSTALL: SKIP: ONEHALFDARK for VIM already installed, skipping ..."
 fi
@@ -170,7 +170,7 @@ fi
 if ! [[ -f "${vim_colors_dir}/onehalflight.vim" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFLIGHT for VIM"
     mkdir -p "${vim_colors_dir}"
-    curl -sSL https://github.com/sonph/onehalf/raw/master/vim/colors/onehalflight.vim -o "${vim_colors_dir}/onehalflight.vim"
+    curl -fL "https://github.com/sonph/onehalf/raw/master/vim/colors/onehalflight.vim" -o "${vim_colors_dir}/onehalflight.vim"
 else
     echo "DOT-INSTALL: SKIP: ONEHALFLIGHT for VIM already installed, skipping ..."
 fi
@@ -179,7 +179,7 @@ fi
 if ! [[ -d "${vim_plugins_dir}/lightline" ]]; then
     echo "DOT-INSTALL: Installing LIGHTLINE for VIM"
     mkdir -p "${vim_plugins_dir}"
-    git clone https://github.com/itchyny/lightline.vim "${vim_plugins_dir}/lightline"
+    git clone "https://github.com/itchyny/lightline.vim" "${vim_plugins_dir}/lightline"
 else
     echo "DOT-INSTALL: SKIP: LIGHTLINE for VIM already installed, skipping ..."
 fi
@@ -204,7 +204,7 @@ fi
 ## install the nvim-web-devicon plugin for nvim
 if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-web-devicons" ]]; then
     echo "DOT-INSTALL: Installing NVIM-WEB-DEVICONS for NVIM"
-    git clone https://github.com/nvim-tree/nvim-web-devicons "${nvim_plugins_dir}/nvim-web-devicons"
+    git clone "https://github.com/nvim-tree/nvim-web-devicons" "${nvim_plugins_dir}/nvim-web-devicons"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-WEB-DEVICONS for NVIM already installed, skipping ..."
 fi
@@ -212,7 +212,7 @@ fi
 ## install the lualine plugin for nvim
 if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/lualine" ]]; then
     echo "DOT-INSTALL: Installing LUALINE for NVIM"
-    git clone https://github.com/nvim-lualine/lualine.nvim.git "${nvim_plugins_dir}/lualine"
+    git clone "https://github.com/nvim-lualine/lualine.nvim.git" "${nvim_plugins_dir}/lualine"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: LUALINE for NVIM already installed, skipping ..."
 fi
@@ -220,7 +220,7 @@ fi
 ## install the nvim-surround plugin for nvim
 if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-surround" ]]; then
     echo "DOT-INSTALL: Installing NVIM-SURROUND for NVIM"
-    git clone https://github.com/kylechui/nvim-surround.git "${nvim_plugins_dir}/nvim-surround"
+    git clone "https://github.com/kylechui/nvim-surround.git" "${nvim_plugins_dir}/nvim-surround"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-SURROUND for NVIM already installed, skipping ..."
 fi
@@ -228,7 +228,7 @@ fi
 ## install the nvim-tree plugin for nvim
 if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-tree" ]]; then
     echo "DOT-INSTALL: Installing NVIM-TREE for NVIM"
-    git clone https://github.com/nvim-tree/nvim-tree.lua.git "${nvim_plugins_dir}/nvim-tree"
+    git clone "https://github.com/nvim-tree/nvim-tree.lua.git" "${nvim_plugins_dir}/nvim-tree"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-TREE for NVIM already installed, skipping ..."
 fi
@@ -236,7 +236,7 @@ fi
 ## install the nvim-lspconfig plugin for nvim
 if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-lspconfig" ]]; then
     echo "DOT-INSTALL: Installing NVIM-LSPCONFIG for NVIM"
-    git clone https://github.com/neovim/nvim-lspconfig.git "${nvim_plugins_dir}/nvim-lspconfig"
+    git clone "https://github.com/neovim/nvim-lspconfig.git" "${nvim_plugins_dir}/nvim-lspconfig"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-LSPCONFIG for NVIM already installed, skipping ..."
 fi
@@ -244,7 +244,7 @@ fi
 ## install the nvim-treesitter plugin for neovim
 if input_contains "--configure-nvim" && [[ ! -d "${nvim_plugins_dir}/nvim-treesitter" ]]; then
     echo "DOT-INSTALL: Installing NVIM-TREESITTER for NVIM"
-    git clone https://github.com/nvim-treesitter/nvim-treesitter.git "${nvim_plugins_dir}/nvim-treesitter"
+    git clone "https://github.com/nvim-treesitter/nvim-treesitter.git" "${nvim_plugins_dir}/nvim-treesitter"
 elif input_contains "--configure-nvim"; then
     echo "DOT-INSTALL: SKIP: NVIM-TREESITTER for NVIM already installed, skipping ..."
 fi
@@ -260,7 +260,7 @@ fi
 ## install onehalfdark for kitty
 if input_contains "--configure-kitty" && [[ ! -f "${kitty_dir}/onehalfdark.conf" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFDARK for KITTY"
-    curl -fsSL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-dark.conf" -o "${kitty_dir}/onehalfdark.conf"
+    curl -fL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-dark.conf" -o "${kitty_dir}/onehalfdark.conf"
 elif input_contains "--configure-kitty"; then
     echo "DOT-INSTALL: SKIP: ONEHALFDARK for KITTY already installed, skipping ..."
 fi
@@ -268,7 +268,7 @@ fi
 ## install onehalfdark for kitty
 if input_contains "--configure-kitty" && [[ ! -f "${kitty_dir}/onehalflight.conf" ]]; then
     echo "DOT-INSTALL: Installing ONEHALFLIGHT for KITTY"
-    curl -fsSL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-light.conf" -o "${kitty_dir}/onehalflight.conf"
+    curl -fL "https://github.com/sonph/onehalf/raw/master/kitty/onehalf-light.conf" -o "${kitty_dir}/onehalflight.conf"
 elif input_contains "--configure-kitty"; then
     echo "DOT-INSTALL: SKIP: ONEHALFLIGHT for KITTY already installed, skipping ..."
 fi
@@ -312,9 +312,9 @@ if input_contains "--install-pyenv" && [[ -d "${HOME}/.pyenv" ]]; then
     exit 1
 elif input_contains "--install-pyenv"; then
     echo "DOT-INSTALL: Installing PYENV"
-    git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
+    git clone "https://github.com/pyenv/pyenv.git" "${HOME}/.pyenv"
     echo "DOT-INSTALL: Intalling PYENV-VIRTUALENV"
-    git clone https://github.com/pyenv/pyenv-virtualenv.git "${HOME}/.pyenv/plugins/pyenv-virtualenv"
+    git clone "https://github.com/pyenv/pyenv-virtualenv.git" "${HOME}/.pyenv/plugins/pyenv-virtualenv"
     echo "DOT-INSTALL: NOTIFICATION: PYENV was installed, but please install all Python dependencies before installing Python ..."
 fi
 
