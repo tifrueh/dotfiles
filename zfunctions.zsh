2d4235d11cafabe265e35ff8d067a730bd3877ab
Timo Früh <dev@tifrueh.ch>
2024-09-03 10:19:12 +0200

    Handle jd-path error codes correctly in jcd

--- zfunctions.zsh
+++ zfunctions.zsh
@@ -1,10 +1,15 @@
 function jcd () {
+    jd_path=$(jd path "$@")
+    jd_path_retval=$?
 
-    if jd_path=$(jd path "$@") && [ -d $jd_path ]; then
+    if [ $jd_path_retval -eq 0 ] && [ -d $jd_path ]; then
         cd $jd_path
-        return 0;
+        return $?
+    elif [ ! -z $jd_path ]; then
+        echo $jd_path
+        return $jd_path_retval
     else
-        return 1;
+        return $jd_path_retval
     fi
 
 }
