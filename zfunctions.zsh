f510c41f48a36eee2f7e28616326171acfbd124e
Timo Früh <dev@tifrueh.ch>
2024-09-13 17:19:38 +0200

    Reflect jd package renaming to jdfs

--- zfunctions.zsh
+++ zfunctions.zsh
@@ -1,15 +1,19 @@
 function jcd () {
-    jd_path=$(jd path "$@")
-    jd_path_retval=$?
 
-    if [ $jd_path_retval -eq 0 ] && [ -d $jd_path ]; then
+    jd_path=$(jdfs path "$@")
+    jdfs_path_retval=$?
+
+    if [ $jdfs_path_retval -eq 0 ] && [ -d $jd_path ]
+    then
         cd $jd_path
         return $?
-    elif [ ! -z $jd_path ]; then
-        echo $jd_path
-        return $jd_path_retval
-    else
-        return $jd_path_retval
-    fi
 
+    elif [ ! -z $jd_path ]
+    then
+        echo $jd_path
+        return $jdfs_path_retval
+
+    else
+        return $jdfs_path_retval
+    fi
 }
