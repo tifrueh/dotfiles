72455d5b6d1dec1c9e32ee507da172aa116513ab
Timo Früh <dev@tifrueh.ch>
2025-07-26 19:20:23 +0200

    Add pcandy script

---
+++ pacman/install.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# Perform script setup.
+source "${0:A:h}/../helpers.zsh"
+setup $0 $@
+
+# Enforce the installation of fastfetch.
+enforce 'less'
+enforce 'pacman'
+enforce 'fzf'
+
+# Link scripts.
+link "${scriptdir}/pcandy.zsh" "${bin_dir}/pcandy" ${force}
---
+++ pacman/pcandy.zsh
@@ -0,0 +1,79 @@
+#!/bin/zsh
+
+hmsg="usage:
+    pcandy [ -h ] [ -i | -f | -a ] search_term
+
+options:
+    -i                  list only installed packages
+    -f                  list only installed and foreign packages
+    -a                  use auracle for search and info
+    search_term         the term to search for
+"
+
+help () {
+    printf '%s' "$hmsg"
+}
+
+display () {
+    preview='pacman --color=always -Si {}'
+    printf '%s' "$2" | fzf --preview "$1" --layout=reverse --bind "enter:execute(${1} | less -R)"
+}
+
+installed () {
+    if [[ $# -lt 1 ]]; then
+        pkgs="$(pacman -Qq)"
+    else
+        q="$1"
+        pkgs="$(pacman -Qsq "$q")"
+    fi
+    display 'pacman --color=always -Qil {}' "$pkgs"
+}
+
+finstalled () {
+    if [[ $# -lt 1 ]]; then
+        pkgs="$(pacman -Qmq)"
+    else
+        printf '%s\n' 'error: cannot search foreign packages'
+        exit 1
+    fi
+    display 'pacman --color=always -Qil {}' "$pkgs"
+}
+
+aur () {
+    if [[ $# -lt 1 ]]; then
+        printf '%s\n' 'error: need search term for auracle'
+        exit 1
+    else
+        q="$1"
+        display \
+            'printf '"'"'%s\n\n'"'"' "$(auracle --color=always info {})" "$(auracle --color=always show {})"' \
+            "$(auracle -q search "$q")"
+    fi
+}
+
+default () {
+    if [[ $# -lt 1 ]]; then
+        pkgs="$(pacman -Slq)"
+    else
+        q="$1"
+        pkgs="$(pacman -Ssq "$q")"
+    fi
+    display 'pacman --color=always -Si {}' "$pkgs"
+}
+
+if [[ "$1" == "-i" ]]; then
+    shift
+    installed $@
+elif [[ "$1" == "-f" ]]; then
+    shift
+    finstalled $@
+elif [[ "$1" == "-a" ]]; then
+    shift
+    aur $@
+elif [[ "$1" == "-h" ]]; then
+    help
+else
+    default $@
+fi
+
+exit 0
