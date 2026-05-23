5bf46b24407cc0fa597f6dbe53a217e3c56232d6
Timo Früh <dev@tifrueh.ch>
2026-05-23 12:54:16 +0200

    [nvim] Improve markdown filetype plugin
    * Use treesitter for folding.
    * Set conceal options.
    * Reorder options alphabetically.

--- nvim/ft-markdown.lua
+++ nvim/ft-markdown.lua
@@ -1,4 +1,9 @@
-vim.bo.textwidth = 80
-vim.wo.spell = true
-vim.bo.spelllang = 'en_gb'
 vim.bo.makeprg = 'pandoc --embed-resources --mathjax --standalone --shift-heading-level-by=-1 -f gfm-implicit_figures -t html5 -o /tmp/pandoc.html "%"'
+vim.bo.spelllang = 'en_gb'
+vim.bo.textwidth = 80
+vim.wo.concealcursor = ''
+vim.wo.conceallevel = 2
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo.foldmethod = 'expr'
+vim.wo.foldlevel = 99
+vim.wo.spell = true
