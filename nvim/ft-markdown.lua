d860379b569f2a98098b98169489ed78e9173610
Timo Früh <dev@tifrueh.ch>
2026-06-09 10:14:10 +0200

    [nvim] Add :Column command for markdown

--- nvim/ft-markdown.lua
+++ nvim/ft-markdown.lua
@@ -7,3 +7,10 @@ vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo.foldmethod = 'expr'
 vim.wo.foldlevel = 99
 vim.wo.spell = true
+
+vim.api.nvim_buf_create_user_command(
+    0,
+    'Column',
+    '<line1>,<line2>!tr -s " " | column -t -s "|" -o "|"',
+    { range = true }
+)
